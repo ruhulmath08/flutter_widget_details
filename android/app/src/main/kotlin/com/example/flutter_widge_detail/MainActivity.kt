b88124a63569c4e0123094c82c3690d152d0d0d1
ruhulmatho8 <ruhul.amin@ctrends-software.com>
2020-07-05 22:53:29 +0600

    git add

--- android/app/src/main/kotlin/com/example/flutter_widge_detail/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_widge_detail
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/routing/routing_constants.dart
+++
@@ -1,4 +0,0 @@
-const String HOME_VIEW_ROUTE = '/';
-const String LOGIN_VIEW_ROUTE = 'login';
-const String MY_BOTTOM_NAVIGATION_ROUTE = 'bottom_navigation';
-const String COLLAPSING_TOOLBAR_ROUTE = 'collapsing_toolbar';
